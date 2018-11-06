187b403372165f157b743e270db2e7b4fc845abe
gaggenwaschke <gaggenwaschke@gmail.com>
2018-11-06 08:52:31 +0100

    Übung 3 fertig gestellt

--- Übung_3/aufgabe_4.c
+++ Übung_3/aufgabe_4.c
@@ -1,34 +1,65 @@
 #include <stdio.h>
 
 void calc_with_float() {
-    float x_1, x_2, x_3, x_4, x_5, result;
-    x_1 = 10000.0;
-    x_2 = -1.0e-3/9.0;
-    x_3 = 25.0e2;
-    x_4 = 1.0e-3;
-    x_5 = -12.5e3;
+    float x[5], result;
+    x[0] = 10000.0;
+    x[1] = -1.0e-3/9.0;
+    x[2] = 25.0e2;
+    x[3] = 1.0e-3/7.0;
+    x[4] = -12.5e3;
 
-    result = x_1 + x_2 + x_3 + x_4 + x_5;
-    printf("with floats: %f\n", result);
+    result = 0.0;
+    for (int i = 0; i < 5; i++) {
+        result += x[i];
+    }
+    printf("with floats: %.10f\n", result);
 }
 
 void calc_with_double() {
-    double x_1, x_2, x_3, x_4, x_5, result;
-    x_1 = 10000.0;
-    x_2 = -1.0e-3/9.0;
-    x_3 = 25.0e2;
-    x_4 = 1.0e-3;
-    x_5 = -12.5e3;
+    double x[5], result;
+    x[0] = 10000.0;
+    x[1] = -1.0e-3/9.0;
+    x[2] = 25.0e2;
+    x[3] = 1.0e-3/7.0;
+    x[4] = -12.5e3;
 
-    result = x_1 + x_2 + x_3 + x_4 + x_5;
-    printf("with doubles: %lf\n", result);
+    result = 0.0;
+    for (int i = 0; i < 5; i++) {
+        result += x[i];
+    }
+    printf("with doubles: %.10lf\n", result);
 }
 
 void calc_alternative() {
-    
+    float x[5], S, S_alt, D;
+    x[0] = 10000.0;
+    x[1] = -1.0e-3/9.0;
+    x[2] = 25.0e2;
+    x[3] = 1.0e-3/7.0;
+    x[4] = -12.5e3;
+
+    S = 0.0;
+    D = 0.0;
+
+    for (int i = 0; i < 5; i++) {
+        S_alt = S;
+        S = S+x[i];
+        D = D + (x[i] - (S - S_alt));
+    }
+
+    S = S+D;
+    printf("with alternative method: %.10f\n", S);
 }
 
 int main() {
+    printf("Hand calculated value = %.10f\n", 3.1746e-5);
     calc_with_float();
     calc_with_double();
+    calc_alternative();
+    // D wird in diesem Verfahren angewandt um die Unterschiede in der Größenordnung,
+    // welche in der Mantisse einer float nicht gespeichert werden können, abzufangen.
+    // Die großen Werte in der Summe werden durch (S-S_alt) maskiert und, wodurch D 
+    // anschließend nur noch die kleineren Stellen in der Mantisse darstellen muss.
+    // Schon beim Rechnen in 3 Größenordnungen würde aber auch dieses Verfahren ungenau.
+    // Außerdem ist es langsamer als das Rechnen mit double-precission.
 }
